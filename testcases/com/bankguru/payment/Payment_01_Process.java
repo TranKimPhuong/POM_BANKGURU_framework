57371c80e5286c4becf6c1c64bd81da3211577e0
admin <admin@win10.localdomain>
2018-11-12 21:10:58 -0800

    test noti on slack

---
+++ testcases/com/bankguru/payment/Payment_01_Process.java
@@ -0,0 +1,19 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Payment_01_Process {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
